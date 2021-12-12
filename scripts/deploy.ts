8183bda33834a79dba7773fb9cf8a6722a48e07f
linnefromice <linnefromice@gmail.com>
2021-12-13 01:59:49 +0900

    modify deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const TodoList = await ethers.getContractFactory("TodoList");
+  const todoList = await TodoList.deploy();
 
-  await greeter.deployed();
+  await todoList.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("todoList deployed to:", todoList.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
