afc25d7df574f1e52e2922f31d24204504d35d7a
linnefromice <linnefromice@gmail.com>
2021-12-13 01:37:55 +0900

    update test - case of createTask

--- test/index.ts
+++ test/index.ts
@@ -9,4 +9,26 @@ describe("TodoList", () => {
 
     expect(await todoList.taskCount()).to.equal(1);
   });
+
+  describe("function createTask", async () => {
+    it("emit event named `Created`", async () => {
+      const TodoList = await ethers.getContractFactory("TodoList");
+      const todoList = await TodoList.deploy();
+      await todoList.deployed();
+
+      await expect(todoList.createTask("Test Task"))
+      .to.emit(todoList, "Created")
+      .withArgs(2, "Test Task");
+    });
+    it("create task", async () => {
+      const TodoList = await ethers.getContractFactory("TodoList");
+      const todoList = await TodoList.deploy();
+      await todoList.deployed();
+
+      await todoList.createTask("first task");
+      expect((await todoList.tasks(2)).content).to.equal("first task");
+      await todoList.createTask("second task");
+      expect((await todoList.tasks(3)).content).to.equal("second task");
+    });
+  });
 });
