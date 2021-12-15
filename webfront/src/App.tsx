416a65103fad87202d6a0bae59e72f35f7114f38
linnefromice <linnefromice@gmail.com>
2021-12-15 09:40:46 +0900

    use contract in frontend - setup provider/contract & call taskCount

--- webfront/src/App.tsx
+++ webfront/src/App.tsx
@@ -1,3 +1,27 @@
-import React, { VFC } from "react";
+import React, { useEffect, useState, VFC } from "react";
+import { ethers } from "ethers";
+import artifact from "./abi/TodoList.json";
 
-export const App: VFC = () => <h1>Hello, TodoList Contract.</h1>
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
+export const App: VFC = () => {
+  const provider = new ethers.providers.JsonRpcProvider();
+  const contract = new ethers.Contract(contractAddress, artifact.abi, provider);
+  const { taskCount } = contract.functions;
+
+  const [taskCountValue, setTaskCountValue] = useState<string>("");
+  useEffect(() => {
+    const getTaskCount = async () => {
+      const _taskCount = await taskCount();
+      setTaskCountValue(_taskCount);
+    }
+    getTaskCount()
+  }, [])
+
+  return (
+    <div>
+      <h1>Hello, TodoList Contract.</h1>
+      <p>{`taskCount ... ${taskCountValue}`}</p>
+    </div>
+  )
+}
