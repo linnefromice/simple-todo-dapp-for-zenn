3119ef97c033e0ff31926468cb7dd1084a95d6bb
linnefromice <linnefromice@gmail.com>
2021-12-16 20:58:28 +0900

    update front - use taskCount

--- webfront/src/App.tsx
+++ webfront/src/App.tsx
@@ -2,26 +2,38 @@ import React, { useEffect, useState, VFC } from "react";
 import { ethers } from "ethers";
 import artifact from "./abi/TodoList.json";
 
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-
-export const App: VFC = () => {
-  const provider = new ethers.providers.JsonRpcProvider();
-  const contract = new ethers.Contract(contractAddress, artifact.abi, provider);
+const useContent = (
+  contract: ethers.Contract
+) => {
   const { taskCount } = contract.functions;
-
   const [taskCountValue, setTaskCountValue] = useState<string>("");
   useEffect(() => {
     const getTaskCount = async () => {
       const _taskCount = await taskCount();
       setTaskCountValue(_taskCount);
     }
-    getTaskCount()
+    getTaskCount();
   }, [])
+
+  return {
+    taskCount: taskCountValue
+  }
+}
+const Content: VFC<{contract: ethers.Contract}> = ({contract}) => {
+  const { taskCount } = useContent(contract);
+  return (<p>{`taskCount ... ${taskCount}`}</p>);
+}
+
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
+export const App: VFC = () => {
+  const provider = new ethers.providers.JsonRpcProvider();
+  const contract = new ethers.Contract(contractAddress, artifact.abi, provider);
 
   return (
     <div>
       <h1>Hello, TodoList Contract.</h1>
-      <p>{`taskCount ... ${taskCountValue}`}</p>
+      <Content contract={contract} />
     </div>
   )
 }
