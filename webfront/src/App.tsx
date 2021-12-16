d7d1e4b33dcf06bbabde013a2388d36d29bc52bd
linnefromice <linnefromice@gmail.com>
2021-12-17 00:28:15 +0900

    update front - use tasks

--- webfront/src/App.tsx
+++ webfront/src/App.tsx
@@ -2,26 +2,66 @@ import React, { useEffect, useState, VFC } from "react";
 import { ethers } from "ethers";
 import artifact from "./abi/TodoList.json";
 
+type Task = {
+  id: string,
+  content: string,
+  isCompleted: boolean
+}
 const useContent = (
   contract: ethers.Contract
 ) => {
-  const { taskCount } = contract.functions;
+  const { taskCount, tasks, createTask } = contract.functions;
   const [taskCountValue, setTaskCountValue] = useState<string>("");
+  const [tasksValue, setTasksValue] = useState<Task[]>([]);
+
   useEffect(() => {
-    const getTaskCount = async () => {
+    const getTasks = async () => {
       const _taskCount = await taskCount();
       setTaskCountValue(_taskCount);
+
+      const _tasks = []
+      for (let i = 1; i <= _taskCount; i++) {
+        const _task = await tasks(i);
+        _tasks.push({
+          ..._task,
+          id: i
+        })
+      }
+      setTasksValue(_tasks);
     }
-    getTaskCount();
+    getTasks();
   }, [])
 
   return {
-    taskCount: taskCountValue
-  }
+    taskCount: taskCountValue,
+    tasks: tasksValue,
+g  }
 }
 const Content: VFC<{contract: ethers.Contract}> = ({contract}) => {
-  const { taskCount } = useContent(contract);
-  return (<p>{`taskCount ... ${taskCount}`}</p>);
+  const { taskCount, tasks } = useContent(contract);
+
+  return (
+    <div>
+      <p>{`taskCount ... ${taskCount}`}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Content</th>
+            <th>Status</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((t, index) => <tr key={`task.${index}`}>
+            <td>{t.id}</td>
+            <td>{t.content}</td>
+            <td>{t.isCompleted ? "Completed" : "Not Completed"}</td>
+          </tr>)}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
