8f3f537a940bd46f9fcd6a33332c3a47c296a2e7
linnefromice <linnefromice@gmail.com>
2021-12-17 00:28:53 +0900

    update frontend - set signer to contract & pass this contract to component

--- webfront/src/App.tsx
+++ webfront/src/App.tsx
@@ -68,12 +68,14 @@ const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 export const App: VFC = () => {
   const provider = new ethers.providers.JsonRpcProvider();
+  const signer = provider.getSigner();
   const contract = new ethers.Contract(contractAddress, artifact.abi, provider);
+  const contractWithSigner = contract.connect(signer);
 
   return (
     <div>
       <h1>Hello, TodoList Contract.</h1>
-      <Content contract={contract} />
+      <Content contract={contractWithSigner} />
     </div>
   )
 }
